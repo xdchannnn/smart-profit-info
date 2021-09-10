45c8589dc0029523d11d6fb69491cfbc8587ade2
Alex-La <55190911+Alex-La@users.noreply.github.com>
2021-09-10 19:22:55 +0300

    Table block translation

--- src/components/homeComponents/tableBlock.js
+++ src/components/homeComponents/tableBlock.js
@@ -1,29 +1,25 @@
-import '../../assets/styles/styles.scoped.css'
-import decor2 from '../../assets/images/decor2.svg';
-import table from '../../assets/images/RU.svg';
+import "../../assets/styles/styles.scoped.css";
+import decor2 from "../../assets/images/decor2.svg";
+import table from "../../assets/images/EN.svg";
+import { useTranslation } from "react-i18next";
 
-function TableBlock(){  
-    
-    return(
-        <section className="table_block">
-        <div className="table_content">
-            <p className="table_title">Таблица ваших доходов</p>
-            <img src={decor2} alt="decor" />
-            <div className="table-responsive">
-            <div className="score_block">
-                <img
-                src={table}
-                alt="table-icon"
-                className="table_img_block"
-                />
-            </div>
-            </div>
-            <p className="table_title">
-            ИЗМЕНИ СВОЮ ЖИЗНЬ ИМЕЯ ВСЕГО 30$ В КАРМАНЕ
-            </p>
+function TableBlock() {
+  const { t } = useTranslation();
+
+  return (
+    <section className="table_block">
+      <div className="table_content">
+        <p className="table_title">{t("landing:TOP_DESCRIPTION_TABLE")}</p>
+        <img src={decor2} alt="decor" />
+        <div className="table-responsive">
+          <div className="score_block">
+            <img src={table} alt="table-icon" className="table_img_block" />
+          </div>
         </div>
-        </section>
-  )
+        <p className="table_title">{t("landing:TOP_DESCRIPTION_POCKET")} </p>
+      </div>
+    </section>
+  );
 }
 
 export default TableBlock;
