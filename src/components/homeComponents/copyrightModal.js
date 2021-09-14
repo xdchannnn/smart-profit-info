9ba4db6aec17e83a8c9a604791430de03c00373a
Alex-La <55190911+Alex-La@users.noreply.github.com>
2021-09-14 22:35:00 +0300

    Update modal orientation

--- src/components/homeComponents/copyrightModal.js
+++ src/components/homeComponents/copyrightModal.js
@@ -1,8 +1,47 @@
 import "../../assets/styles/styles.scoped.css";
 import { useTranslation } from "react-i18next";
+import { useEffect, useState } from "react";
 
 function CopyrightModal() {
   const { t } = useTranslation();
+
+  const [height, setHeight] = useState(window.innerHeight);
+  const [orientation, setOrientation] = useState(
+    (window.screen.orientation || {}).type ||
+      window.screen.mozOrientation ||
+      window.screen.msOrientation
+  );
+  const [modalHeight, setModalHeight] = useState("100%");
+
+  const updateHeight = () => setHeight(window.innerHeight);
+  const updateOrientation = () =>
+    setOrientation(
+      (window.screen.orientation || {}).type ||
+        window.screen.mozOrientation ||
+        window.screen.msOrientation
+    );
+
+  useEffect(() => {
+    window.addEventListener("resize", updateHeight);
+    window.addEventListener("orientationchange", updateOrientation);
+    return () => {
+      window.removeEventListener("resize", updateHeight);
+      window.removeEventListener("orientationchange", updateOrientation);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log(orientation, height);
+  }, [orientation, height]);
+
+  useEffect(() => {
+    if (orientation === "landscape-primary") {
+      if (height <= 320) setModalHeight(300);
+      else if (height <= 360) setModalHeight(340);
+      else if (height <= 375) setModalHeight(355);
+      else if (height <= 414) setModalHeight(391);
+    } else setModalHeight("100%");
+  }, [orientation]);
 
   return (
     <div
@@ -12,13 +51,17 @@ function CopyrightModal() {
       aria-labelledby="copyright_modalLabel"
       aria-hidden="true"
     >
-      <div className="modal-dialog modal-dialog-scrollable">
+      <div
+        className="modal-dialog modal-dialog-scrollable"
+        style={{ height: modalHeight }}
+      >
         <div className="modal-content">
           <div className="modal-header" style={{ backgroundColor: "#f2ca6b" }}>
             <h5 className="modal-title" id="copyright_modalLabel">
               {t("footer:TOP_DESCRIPTION_RULESANDAGREEMENTS")}
             </h5>
             <button
+              style={{ margin: 0 }}
               type="button"
               className="btn-close"
               data-bs-dismiss="modal"
