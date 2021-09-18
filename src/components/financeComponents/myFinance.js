1d79f14cac58529ba1c596f4a742150f612cbc0f
Alex-La <55190911+Alex-La@users.noreply.github.com>
2021-09-18 06:02:53 +0300

    My finance translation

--- src/components/financeComponents/myFinance.js
+++ src/components/financeComponents/myFinance.js
@@ -1,4 +1,5 @@
 import { useContext, useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import "../../assets/styles/finance.scoped.css";
 import "../../assets/styles/general.scoped.css";
 import AuthContext from "../../context/auth.context";
@@ -6,6 +7,8 @@ import AuthContext from "../../context/auth.context";
 import useFetch from "../../hooks/useFetch.hook";
 
 function MyFinance() {
+  const { t } = useTranslation();
+
   const { token } = useContext(AuthContext);
   const { request, error } = useFetch();
 
@@ -24,50 +27,54 @@ function MyFinance() {
         <tbody>
           <tr>
             <td className="main_row">
-              <p>Номер транзакции</p>
+              <p>{t("finance:TOP_DESCRIPTION_TRANSACTION")}</p>
             </td>
             <td className="main_row">
-              <p>От кого</p>
+              <p>{t("finance:TOP_DESCRIPTION_FROMWHON")}</p>
             </td>
             <td className="main_row">
-              <p>Уровень</p>
+              <p>{t("finance:TOP_DESCRIPTION_LEVEL3")}</p>
             </td>
             <td className="main_row">
-              <p>TRX</p>
+              <p>{t("finance:TOP_DESCRIPTION_BNB2")}</p>
             </td>
             <td className="main_row">
-              <p>Дата</p>
+              <p>{t("finance:TOP_DESCRIPTION_DATE3")}</p>
             </td>
             <td className="main_row">
-              <p>Статус</p>
+              <p>{t("finance:TOP_DESCRIPTION_STATUS3")}</p>
             </td>
           </tr>
-          <tr className="child_one">
-            <td className="child_row">
-              <p>1500</p>
-            </td>
-            <td className="child_row">
-              <p>
-                <span className="green_text">FP:</span> ID 56908
-              </p>
-            </td>
-            <td className="child_row">
-              <p>Реферальный</p>
-            </td>
-            <td className="child_row">
-              <p>3500</p>
-            </td>
-            <td className="child_row">
-              <p className="date_text">02.07.2021</p>
-            </td>
-            <td className="child_row">
-              <p className="success_text">Выполнен</p>
-            </td>
-          </tr>
+          {Array(25)
+            .fill()
+            .map((item, index) => (
+              <TableItem item={item} t={t} key={index} />
+            ))}
         </tbody>
       </table>
     </div>
   );
 }
+
+const TableItem = ({ item, t }) => (
+  <tr className="child_one">
+    <td className="child_row">{item && <p>1500</p>}</td>
+    <td className="child_row">
+      {item && (
+        <p>
+          <span className="green_text">FP:</span> ID 56908
+        </p>
+      )}
+    </td>
+    <td className="child_row">{item && <p>Реферальный</p>}</td>
+    <td className="child_row">{item && <p>3500</p>}</td>
+    <td className="child_row">
+      {item && <p className="date_text">02.07.2021</p>}
+    </td>
+    <td className="child_row">
+      {item && <p className="success_text">Выполнен</p>}
+    </td>
+  </tr>
+);
 
 export default MyFinance;
