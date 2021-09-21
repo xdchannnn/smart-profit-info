84b4cb136bdadc57e49732d689883d83a186489a
Alex-La <55190911+Alex-La@users.noreply.github.com>
2021-09-21 23:06:35 +0300

    Get maxi bonus

--- src/components/homeComponents/mainPage.js
+++ src/components/homeComponents/mainPage.js
@@ -1,42 +1,54 @@
+import { useEffect } from "react";
 import "../../assets/styles/styles.scoped.css";
 import mainLogo from "../../assets/images/main-logo.svg";
 import { useTranslation } from "react-i18next";
 import Particles from "react-particles-js";
 
 import config from "../../particlesjs-config.json";
+import useBonus from "../../hooks/web3/bonus.hook";
+
+import Preloader from "../loaders/Preloader";
 
 function MainPage() {
   const { t } = useTranslation();
+  const { loading, getMaxiBonus, maxiBonus } = useBonus();
+
+  useEffect(() => {
+    getMaxiBonus();
+  }, []);
 
   return (
-    <section className="main_page" style={{ position: "relative" }}>
-      <Particles style={{ position: "absolute" }} params={config} />
-      <div className="main_content">
-        <div className="top_text">
-          <p className="top_title">{t("landing:TOP_TITLE")}</p>
-          <p className="top_description">{t("landing:TOP_DESCRIPTION")} </p>
+    <>
+      {loading && <Preloader />}
+      <section className="main_page" style={{ position: "relative" }}>
+        <Particles style={{ position: "absolute" }} params={config} />
+        <div className="main_content">
+          <div className="top_text">
+            <p className="top_title">{t("landing:TOP_TITLE")}</p>
+            <p className="top_description">{t("landing:TOP_DESCRIPTION")} </p>
+          </div>
+          <div className="color_delimiter" />
+          <div className="main_center">
+            <img src={mainLogo} alt="main-logo" className="main_logo" />
+          </div>
+          <div className="color_delimiter" />
+          <div className="bottom_text">
+            <p className="bottom_description">
+              {t("landing:TOP_DESCRIPTION_MANY")}
+            </p>
+            <p className="bottom_title">
+              {t("landing:TOP_DESCRIPTION_MANY_YEARS")}
+            </p>
+          </div>
+          <div className="main_banner">
+            <p className="main_banner_title">
+              {t("landing:TOP_DESCRIPTION_MAXI")}
+            </p>
+            <p className="main_banner_description">{maxiBonus} BNB</p>
+          </div>
         </div>
-        <div className="color_delimiter" />
-        <div className="main_center">
-          <img src={mainLogo} alt="main-logo" className="main_logo" />
-        </div>
-        <div className="color_delimiter" />
-        <div className="bottom_text">
-          <p className="bottom_description">
-            {t("landing:TOP_DESCRIPTION_MANY")}
-          </p>
-          <p className="bottom_title">
-            {t("landing:TOP_DESCRIPTION_MANY_YEARS")}
-          </p>
-        </div>
-        <div className="main_banner">
-          <p className="main_banner_title">
-            {t("landing:TOP_DESCRIPTION_MAXI")}
-          </p>
-          <p className="main_banner_description">32 540 BNB</p>
-        </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
 
