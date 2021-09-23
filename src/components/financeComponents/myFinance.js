7863875113e48eb9ba353c5ef8bcb819b3acde95
Alex-La <55190911+Alex-La@users.noreply.github.com>
2021-09-23 16:45:47 +0300

    Update myFinance.js

--- src/components/financeComponents/myFinance.js
+++ src/components/financeComponents/myFinance.js
@@ -1,17 +1,19 @@
 import { useContext, useEffect, useState } from "react";
 import { useSSR, useTranslation } from "react-i18next";
+import { toast } from "react-toastify";
 import "../../assets/styles/finance.scoped.css";
 import "../../assets/styles/general.scoped.css";
 import AuthContext from "../../context/auth.context";
 
 import useFetch from "../../hooks/useFetch.hook";
 import usePrice from "../../hooks/web3/price.hook";
+import Preloader from "../loaders/Preloader";
 
 function MyFinance() {
   const { t } = useTranslation();
 
   const { token } = useContext(AuthContext);
-  const { request, error } = useFetch();
+  const { request, error, loading, clearError } = useFetch();
   const { getLatestPrice, latestPrice } = usePrice();
 
   const [data, setData] = useState();
@@ -37,34 +39,44 @@ function MyFinance() {
     })();
   }, [request]);
 
+  useEffect(() => {
+    if (error) {
+      toast(error.message, { type: "error" });
+      clearError();
+    }
+  }, [error]);
+
   return (
-    <div id="MyFinance" className="tabcontent">
-      <table className="general_table">
-        <tbody>
-          <tr>
-            <td className="main_row">
-              <p>{t("finance:TOP_DESCRIPTION_TRANSACTION")}</p>
-            </td>
-            <td className="main_row">
-              <p>{t("finance:TOP_DESCRIPTION_FROMWHON")}</p>
-            </td>
-            <td className="main_row">
-              <p>{t("finance:TOP_DESCRIPTION_LEVEL3")}</p>
-            </td>
-            <td className="main_row">
-              <p>USD - {t("finance:TOP_DESCRIPTION_BNB2")}</p>
-            </td>
-            <td className="main_row">
-              <p>{t("finance:TOP_DESCRIPTION_DATE3")}</p>
-            </td>
-          </tr>
-          {data &&
-            data.map((item, index) => (
-              <TableItem item={item} latestPrice={latestPrice} key={index} />
-            ))}
-        </tbody>
-      </table>
-    </div>
+    <>
+      {loading && <Preloader />}
+      <div id="MyFinance" className="tabcontent">
+        <table className="general_table">
+          <tbody>
+            <tr>
+              <td className="main_row">
+                <p>{t("finance:TOP_DESCRIPTION_TRANSACTION")}</p>
+              </td>
+              <td className="main_row">
+                <p>{t("finance:TOP_DESCRIPTION_FROMWHON")}</p>
+              </td>
+              <td className="main_row">
+                <p>{t("finance:TOP_DESCRIPTION_LEVEL3")}</p>
+              </td>
+              <td className="main_row">
+                <p>USD - {t("finance:TOP_DESCRIPTION_BNB2")}</p>
+              </td>
+              <td className="main_row">
+                <p>{t("finance:TOP_DESCRIPTION_DATE3")}</p>
+              </td>
+            </tr>
+            {data &&
+              data.map((item, index) => (
+                <TableItem item={item} latestPrice={latestPrice} key={index} />
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
