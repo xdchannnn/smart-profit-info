9c3ba192b4e1f1f223378cbce774a5ca15629ccd
Alex-La <55190911+Alex-La@users.noreply.github.com>
2021-09-25 08:46:05 +0300

    Update paidPartners.js

--- src/components/teamComponents/paidPartners.js
+++ src/components/teamComponents/paidPartners.js
@@ -1,7 +1,7 @@
 import "../../assets/styles/general.scoped.css";
 import "../../assets/styles/my-team.scoped.css";
 
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, useMemo } from "react";
 import useFetch from "../../hooks/useFetch.hook";
 import AuthContext from "../../context/auth.context";
 import { useTranslation } from "react-i18next";
@@ -82,18 +82,47 @@ function PaidPartners() {
 }
 
 function Table({ item, t }) {
+  const renderStatus = useMemo(() => {
+    if (item) {
+      switch (item.status) {
+        case 0:
+          return <span className="blue_text">SP:</span>;
+        case 1:
+          return <span className="green_text">FP:</span>;
+        case 2:
+          return <span className="yellow_text">MP:</span>;
+        case 3:
+          return <span className="purple_text">F:</span>;
+      }
+    } else return <p />;
+  }, []);
+
+  const renderFullStatus = useMemo(() => {
+    if (item) {
+      switch (item.status) {
+        case 0:
+          return <span className="blue_text">Start Profit</span>;
+        case 1:
+          return <span className="green_text">Fixed Profit</span>;
+        case 2:
+          return <span className="yellow_text">Maxi Profit</span>;
+        case 3:
+          return <span className="purple_text">Free</span>;
+      }
+    } else return <p />;
+  }, []);
+
   return (
     <tr className="child_one">
       <td className="child_row">
-        <p>{item && item.name}</p>
+        <p>{item && item.full_name}</p>
       </td>
       <td className="child_row">
         <div className="child_content">
           {item && (
             <>
               <p>
-                <span className="yellow_text">{item && item.status}:</span> ID{" "}
-                {item && item.id}
+                {renderStatus} ID {item && item.id}
               </p>
               <div className="popover__wrapper">
                 <a href="#">
@@ -106,7 +135,7 @@ function Table({ item, t }) {
                   <div className="user_information">
                     <p className="status_item">
                       {t("myteam:TOP_DESCRIPTION_STATUS1")}:{" "}
-                      <span className="status_text">{item && item.status}</span>
+                      <span className="status_text">{renderFullStatus}</span>
                     </p>
                     <p className="country_id">
                       {t("myteam:TOP_DESCRIPTION_COUNTRY1")}:{" "}
@@ -148,7 +177,7 @@ function Table({ item, t }) {
         <p>{item && item.team_count}</p>
       </td>
       <td className="child_row">
-        <p>{item && item.paid_date}</p>
+        <p>{item && new Date(item.paid_date * 1000).toLocaleDateString()}</p>
       </td>
     </tr>
   );
